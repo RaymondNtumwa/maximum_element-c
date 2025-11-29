70b087841086f13f3f1f6d08f79fb983dcdca634
RaymondNtumwa <martianrhae@gmail.com>
2025-11-29 16:05:37 +0300

    Refactor max element program with new array

--- max_element.c
+++ max_element.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-
-// Define the size of the array
 #define ARRAY_SIZE 10
 
-int main() {
-    // 1. Initialize Array
-    int data_array[ARRAY_SIZE] = {18, 45, 9, 72, 30, 99, 51, 6, 88, 25};
-    int max_element;
+int main(){
+    int arr[ARRAY_SIZE] = {18, 22, 45, 12, 67, 34, 89, 23, 56, 78};
+    int max;
 
-    // Display the array contents for context
-    printf("The current array elements are:\n");
-    printf("[");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d%s", data_array[i], (i == ARRAY_SIZE - 1) ? "" : ", ");
+    printf("ARRAY ELEMENTS:\n");
+    printf("---------------------------------------\n");
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("%d%s", arr[i], (i < ARRAY_SIZE - 1) ? ", " : "\n");
     }
-    printf("]\n");
+    printf("---------------------------------------\n");
+    
+    max = arr[0];
+    for(int i = 1; i < ARRAY_SIZE; i++){
+        if (arr[i] > max){
+            max = arr[i];
 
-    max_element = data_array[0];
-
-    for (int i = 1; i < ARRAY_SIZE; i++) {
-        if (data_array[i] > max_element) {
-            max_element = data_array[i];
         }
     }
-
-    printf("maximum element: %d\n", max_element);
+    printf("MAX ELEMENT: %d\n", max);
 
     return 0;
 }
